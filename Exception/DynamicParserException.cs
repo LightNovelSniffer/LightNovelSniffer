2034be5a497e9b01dcaeceffd53a1ae5873b1155
JoshuaArus <joshua.arus@gmail.com>
2018-04-11 14:30:31 +0200

    Adding ParserException and DynamicParserException

---
+++ Exception/DynamicParserException.cs
@@ -0,0 +1,10 @@
+﻿namespace LightNovelSniffer.Exception
+{
+    public class DynamicParserException : System.Exception
+    {
+        public DynamicParserException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ Exception/ParserException.cs
@@ -0,0 +1,9 @@
+﻿namespace LightNovelSniffer.Exception
+{
+    public class ParserException : System.Exception
+    {
+        public ParserException(string message, System.Exception e) : base(message, e)
+        {
+        }
+    }
+}
